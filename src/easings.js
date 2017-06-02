88448ec7c5824a3ffdea99cf2c93e869178bed90
Clément Pasquier <clement@n00sphere.net>
2017-06-02 16:33:37 +0200

    1.0.4

--- src/easings.js
+++ src/easings.js
@@ -1,6 +1,11 @@
-export const EaseIn  = function(power){return function(t){return Math.pow(t, power)}};
-export const EaseOut = function(power){return function(t){return 1 - Math.abs(Math.pow(t-1, power))}};
-export const EaseInOut = function(power){return function(t){return t<.5 ? EaseIn(power)(t*2)/2 : EaseOut(power)(t*2 - 1)/2+0.5}};
+export const EaseIn  = power => t => Math.pow(t, power);
+export const EaseOut = power => t => 1 - Math.abs(Math.pow(t - 1, power));
+export const EaseInOut = power => t =>
+  t < .5
+    ? EaseIn(power)(t * 2) / 2
+    : EaseOut(power)(t * 2 - 1) / 2 + 0.5;
+
+
 export const easings = {
   linear: EaseInOut(1),
   easeInQuad: EaseIn(2),
@@ -14,7 +19,11 @@ export const easings = {
   easeInOutQuart: EaseInOut(4),
   easeInQuint: EaseIn(5),
   easeOutQuint: EaseOut(5),
-  easeInOutQuint: EaseInOut(5)
+  easeInOutQuint: EaseInOut(5),
+  easeInCirc: t => -(Math.sqrt(1 - easings.easeInQuad(t)) - 1),
+  easeOutCirc: t => Math.sqrt(easings.easeOutQuad(t))
 };
+
+
 
 
